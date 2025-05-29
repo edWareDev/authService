1d5e9c569868f806669ff1fd1cd1f665871ef69d
Edward Espinoza <em.esc.et@gmail.com>
2025-05-29 10:52:05 -0500

    REFACTOR:: Enhance refresh token handling in authentication flow

--- src/adapters/routers/authRouter.js
+++ src/adapters/routers/authRouter.js
@@ -1,16 +1,16 @@
 import { Router } from "express";
-import { controllerLogin, controllerLogout } from "../web/controllers/AuthController.js";
+import { controllerLogin, controllerLogout, controllerRefreshAccessToken } from "../web/controllers/AuthController.js";
 
 
 export const authRouter = Router()
 
-// RUTAS POST LOGIN
+// RUTAS POST 
+// LOGIN
 authRouter.post('/login', controllerLogin)
 
-// RUTAS POST LOGIN
-authRouter.delete('/logout', controllerLogout)
-// RUTAS PATCH
-// authRouter.patch('/id/:Id', controllerUpdateUser)
+// REFRESH TOKEN
+authRouter.post('/refreshAccessToken', controllerRefreshAccessToken)
 
 // RUTAS DELETE
-// authRouter.delete('/id/:Id', controllerDeleteUser)
+// LOGOUT
+authRouter.delete('/logout', controllerLogout)
--- src/adapters/web/controllers/AuthController.js
+++ src/adapters/web/controllers/AuthController.js
@@ -1,4 +1,5 @@
 import { verifyRefreshToken } from "../../../infraestructure/security/jwtService.js";
+import { createAccessToken } from "../../../usecases/accessToken/CreateAccessToken.js";
 import { disableRefreshToken } from "../../../usecases/refreshToken/DisableRefreshToken.js";
 import { authenticateUser } from "../../../usecases/users/AuthenticateUser.js";
 import { CustomError } from "../../../utils/CustomError.js";
@@ -16,7 +17,30 @@ export async function controllerLogin(req, res) {
             sameSite: 'None',
             path: '/auth'
         });
-        fetchResponse(res, { statusCode: 200, message: 'Sesión iniciada correctamente.', data: authenticationData.accessToken });
+        fetchResponse(res, { statusCode: 200, message: 'Sesión iniciada correctamente.', data: { accessToken: authenticationData.accessToken } });
+    } catch (error) {
+        if (error instanceof CustomError) {
+            const { message, httpErrorCode, errorCode } = error.toJSON();
+            fetchResponse(res, { statusCode: httpErrorCode, message, errorCode });
+        } else {
+            fetchResponse(res, { statusCode: 500, errorCode: "ERR_UNEXPECTED", message: "Ha ocurrido un error inesperado" });
+        }
+    }
+}
+
+export async function controllerRefreshAccessToken(req, res) {
+    try {
+        const refreshToken = req.cookies?.refreshToken;
+        if (!refreshToken || refreshToken === '') throw new CustomError('Hubo un error al cerrar la sesión', 400, "El refresh token no fue encontrado.");
+
+        const tokenData = verifyRefreshToken(refreshToken);
+        console.log("🚀 ~ controllerRefreshToken ~ tokenData:", tokenData)
+        if (!tokenData || tokenData.error) throw new CustomError('Hubo un error al refrescar el token', 400, [tokenData?.error || "El refresh token es inválido"]);
+
+        const accessToken = createAccessToken({ userId: tokenData.sub, systemId: tokenData.aud });
+        if (!accessToken || accessToken.error) throw new CustomError('Hubo un error al refrescar el token', 400, [tokenData?.error || "No fue posible crear el access token"]);
+
+        fetchResponse(res, { statusCode: 200, message: 'Sesión iniciada correctamente.', data: { accessToken } });
     } catch (error) {
         if (error instanceof CustomError) {
             const { message, httpErrorCode, errorCode } = error.toJSON();
@@ -30,13 +54,13 @@ export async function controllerLogin(req, res) {
 export async function controllerLogout(req, res) {
     try {
         const refreshToken = req.cookies?.refreshToken;
-        if (!refreshToken) CustomError('Hubo un error al cerrar la sesión', 400, "ERR_REFRESH_TOKEN_MISSING");
+        if (!refreshToken || refreshToken === '') throw new CustomError('Hubo un error al cerrar la sesión', 400, "El refresh token no fue encontrado.");
 
         const tokenData = verifyRefreshToken(refreshToken);
         if (!tokenData || tokenData.error) throw new CustomError('Hubo un error al cerrar la sesión', 400, [tokenData?.error || "El rfresh token es inválido"]);
 
         const disabledToken = await disableRefreshToken(refreshToken);
-        if (!disabledToken || disabledToken.error) CustomError("No se pudo desactivar el refresh token.", 400, "El token ya fue desactivado o no existe.");
+        if (!disabledToken || disabledToken.error) throw new CustomError("No se pudo desactivar el refresh token.", 400, "El token ya fue desactivado o no existe.");
 
         res.clearCookie('refreshToken', {
             httpOnly: true,
